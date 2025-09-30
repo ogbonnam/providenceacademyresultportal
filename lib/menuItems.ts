18912dd8408cd14a3b643ed18cd5d9de68cb60b2
george <georgeenueshike@gmail.com>
2025-09-30 15:06:32 +0100

    removed some menu items to be added later

--- lib/menuItems.ts
+++ lib/menuItems.ts
@@ -30,9 +30,9 @@ export const adminMenuItems = [
     icon: MdOutlineSchool,
     submenus: [
       { name: "Add Teacher", href: "/admin/teachers/add" },
-      { name: "View Teachers", href: "/admin/teachers/view" },
+      // { name: "View Teachers", href: "/admin/teachers/view" },
       { name: "Assign to Subject", href: "/admin/teachers/assign-subjects" },
-      { name: "Pause Teacher", href: "/admin/teachers/pause" },
+      // { name: "Pause Teacher", href: "/admin/teachers/pause" },
     ],
   },
   {
@@ -44,24 +44,24 @@ export const adminMenuItems = [
       { name: "Pause parent", href: "/admin/parents/deactivated" },
     ],
   },
-  {
-    name: "Admins",
-    icon: MdOutlinePerson,
-    submenus: [{ name: "Add Admins", href: "/admin/admins/add" }],
-  },
+  // {
+  //   name: "Admins",
+  //   icon: MdOutlinePerson,
+  //   submenus: [{ name: "Add Admins", href: "/admin/admins/add" }],
+  // },
   {
     name: "Subjects/Classes",
     icon: MdOutlineLibraryBooks,
     submenus: [
       { name: "Add Subject", href: "/admin/subjects/add" },
-      { name: "View Subjects", href: "/admin/subjects/view" },
+      // { name: "View Subjects", href: "/admin/subjects/view" },
       { name: "Assign Students", href: "/admin/subjects/assign-students" },
-      { name: "Add Foundation Year", href: "/admin/subjects/add-year" },
-      { name: "Add Foundation Class", href: "/admin/subjects/add-class" },
-      {
-        name: "Assign Students to Class",
-        href: "/admin/subjects/assign-class",
-      },
+      // { name: "Add Foundation Year", href: "/admin/subjects/add-year" },
+      // { name: "Add Foundation Class", href: "/admin/subjects/add-class" },
+      // {
+      //   name: "Assign Students to Class",
+      //   href: "/admin/subjects/assign-class",
+      // },
     ],
   },
   {
@@ -85,7 +85,7 @@ export const adminMenuItems = [
     icon: MdOutlineBarChart,
     submenus: [
       { name: "Video Content", href: "/admin/videos" },
-      { name: "Book Content", href: "/admin/reports" },
+      // { name: "Book Content", href: "/admin/reports" },
     ],
   },
 ];
